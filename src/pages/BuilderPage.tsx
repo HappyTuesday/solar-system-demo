61d6d23433c96bff6b801fe6806ca6aff3c3f11f
mylittleorange <1404976776@qq.com>
2026-06-20 16:09:28 +0800

    feat: assemble BuilderPage with Canvas 2D and all builder components

--- src/pages/BuilderPage.tsx
+++ src/pages/BuilderPage.tsx
@@ -1,11 +1,35 @@
+import { useKeyboardShortcuts } from '../hooks/useKeyboardShortcuts';
+import CelestialToolbar from '../components/builder/CelestialToolbar';
+import BuilderCanvas from '../components/builder/BuilderCanvas';
+import CoordinateDisplay from '../components/builder/CoordinateDisplay';
+import BodyStatusPanel from '../components/builder/BodyStatusPanel';
+import CloseApproachOverlay from '../components/builder/CloseApproachOverlay';
+import ControlPanel from '../components/builder/ControlPanel';
+import HistoryPanel from '../components/builder/HistoryPanel';
+import ScoreModal from '../components/builder/ScoreModal';
 import './BuilderPage.css';
 
 function BuilderPage() {
+  useKeyboardShortcuts();
+
   return (
     <div className="builder-page">
-      <div className="builder-placeholder">
-        <p>搭建页面 — 重构中</p>
+      <div className="builder-panel-left">
+        <CelestialToolbar />
       </div>
+      <div className="builder-panel-center">
+        <div className="builder-canvas-wrapper">
+          <BuilderCanvas />
+        </div>
+        <CoordinateDisplay />
+        <CloseApproachOverlay />
+        <BodyStatusPanel />
+      </div>
+      <div className="builder-panel-right">
+        <ControlPanel />
+        <HistoryPanel />
+      </div>
+      <ScoreModal />
     </div>
   );
 }
