7d33cb29a8062ee77830895ae829a9d1a1d8645b
mylittleorange <1404976776@qq.com>
2026-06-27 07:27:06 +0800

    feat: integrate HUD into ExplorePage

--- src/pages/ExplorePage.tsx
+++ src/pages/ExplorePage.tsx
@@ -1,5 +1,6 @@
 import ExploreCanvas from '../components/explore/ExploreCanvas';
 import Dashboard from '../components/explore/Dashboard';
+import HUD from '../components/explore/HUD';
 import CrashOverlay from '../components/explore/CrashOverlay';
 import './ExplorePage.css';
 
@@ -9,6 +10,7 @@ function ExplorePage() {
       <div className="explore-canvas-area">
         <ExploreCanvas />
       </div>
+      <HUD />
       <Dashboard />
       <CrashOverlay />
     </div>
